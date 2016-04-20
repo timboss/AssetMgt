63009493b98c3c618915a1d229e98693d4c58984
timboss <git@timboss.com>
2016-04-20 17:32:16 +0100

    Working EditAsset form and link from AssetDetails

--- assetregister/urls.py
+++ assetregister/urls.py
@@ -6,5 +6,7 @@ from django.conf import settings
 urlpatterns = [
     url(r'^$', views.asset_list, name="asset_list"),
     url(r'^asset/(?P<pk>\d+)/$', views.asset_detail, name="asset_detail"),
+    url(r'^asset/new/$', views.asset_new, name="asset_new"),
+    url(r'^asset/(?P<pk>\d+)/edit/$', views.asset_edit, name='asset_edit'),
 ]
 
--- assetregister/views.py
+++ assetregister/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from .models import Asset
+from .forms import EditAsset
 
 # Create your views here.
 
 def asset_list(request):
+    asset_count = Asset.objects.count()
+    active_asset_count = Asset.objects.filter(asset_status=1).count()
+    assets = Asset.objects.order_by("asset_id")
+    return render(request, "assetregister/asset_list.html", {
+        "assets": assets, "asset_count": asset_count, "active_asset_count": active_asset_count
+        }) 
+
+def active_asset_list(request):
     asset_count = Asset.objects.count()
     active_asset_count = Asset.objects.filter(asset_status=1).count()
     assets = Asset.objects.filter(asset_status=1).order_by("asset_id")
@@ -16,3 +26,32 @@ def asset_detail(request, pk):
     #ToDo - "ParentOf" field
     #     - Status number -> words translation
     return render(request, "assetregister/asset_details.html", {"asset": asset})
+
+
+def asset_new(request):
+    if request.method == "POST":
+        form = EditAsset(request.POST, request.FILES)
+        if form.is_valid():
+            asset = form.save(commit=False)
+            asset.edited_by = request.user
+            asset.edited_on = timezone.now()
+            asset.save()
+            return redirect("asset_detail", pk=asset.pk)
+    else:
+        form = EditAsset()
+    return render(request, "assetregister/asset_edit.html", {"form": form})
+
+
+def asset_edit(request, pk):
+    asset = get_object_or_404(Asset, pk=pk)
+    if request.method == "POST":
+        form = EditAsset(request.POST, request.FILES, instance=asset)
+        if form.is_valid():
+            asset = form.save(commit=False)
+            asset.edited_by = request.user
+            asset.edited_on = timezone.now()
+            asset.save()
+            return redirect("asset_detail", pk=asset.pk)
+    else:
+        form = EditAsset(instance=asset)
+    return render(request, "assetregister/asset_edit.html", {"form": form})
