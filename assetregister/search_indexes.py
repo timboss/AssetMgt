87c1fbf5675b8ba7c7dfca229cd2133abf0397e6
timboss <git@timboss.com>
2016-10-17 11:03:39 +0100

    Updated search results to include manufacturer and location. Changed Whoosh index to have more model fields available to filter on (moved out of 'document' field)

--- assetregister/search_indexes.py
+++ assetregister/search_indexes.py
@@ -5,15 +5,15 @@ from .models import Asset
 class AssetIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.EdgeNgramField(document=True, use_template=True)
     #'text' "doc & template = true" uses asset_text.txt data template
-    asset_description = indexes.CharField(model_attr='asset_description')
-    #manufacturer = indexes.CharField(model_attr='asset_manufacturer')
-    #model = indexes.CharField(model_attr='asset_model')
+    #asset_description = indexes.CharField(model_attr='asset_description')
+    manufacturer = indexes.CharField(model_attr='asset_manufacturer')
+    model = indexes.CharField(model_attr='asset_model')
     #serial_no = indexes.CharField(model_attr='asset_serial_number')
-    #person_responsible = indexes.CharField(model_attr='person_responsible')
+    person_responsible = indexes.CharField(model_attr='person_responsible')
     #person_responsible_email = indexes.CharField(model_attr='person_responsible_email')
     #purchase_order_ref = indexes.CharField(model_attr='purchase_order_ref')
     #funded_by = indexes.CharField(model_attr='funded_by')
-    #asset_location_building = indexes.CharField(model_attr='asset_location_building')
+    asset_location_building = indexes.CharField(model_attr='asset_location_building')
     #asset_location_room = indexes.CharField(model_attr='asset_location_room')
     # ^ All the above is now stored as one field "text", see asset_text.txt data template doc
 
