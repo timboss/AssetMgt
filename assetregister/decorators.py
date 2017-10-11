102acd3088b89326107d9b6cd2576f7ee672e400
Tim Jones <github@timboss.com>
2017-10-11 13:03:26 +0100

    Bulk reserve assets form

--- assetregister/decorators.py
+++ assetregister/decorators.py
@@ -4,7 +4,7 @@ def group_required(*group_names):
     """Requires user membership in at least one of the groups passed in."""
     def in_groups(u):
         if u.is_authenticated():
-            if bool(u.groups.filter(name__in=group_names)) | u.is_superuser:
+            if bool(u.groups.filter(name__in=group_names)) or bool(u.groups.filter(name=="SuperUsers")):
                 return True
         return False
     return user_passes_test(in_groups)
